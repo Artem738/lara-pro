2abee16c26a8b3702838785c2de05eedbae695e8
ArtRyzen <artembabichev@gmail.com>
2023-07-21 00:30:18 +0300

    HW #9 - indexes

--- database/migrations/2023_07_20_212244_add_index_to_books_table_created_at_category_id.php
+++ database/migrations/2023_07_20_212244_add_index_to_books_table_created_at_category_id.php
@@ -24,7 +24,7 @@ return new class extends Migration {
     {
         Schema::table(
             'books', function (Blueprint $table) {
-            //
+            $table->dropIndex(['created_at', 'category_id']);
         }
         );
     }

513ee344b18c9f88325da29fd4be8a09c0fdd569
ArtRyzen <artembabichev@gmail.com>
2023-07-21 00:24:57 +0300

    HW #9 - indexes

--- database/migrations/2023_07_20_212244_add_index_to_books_table_created_at_category_id.php
+++ database/migrations/2023_07_20_212244_add_index_to_books_table_created_at_category_id.php
@@ -4,17 +4,17 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::table('books', function (Blueprint $table) {
-            $table->index('year');
-
-        });
+        Schema::table(
+            'books', function (Blueprint $table) {
+            $table->index(['created_at', 'category_id']);
+        }
+        );
     }
 
     /**
@@ -22,8 +22,10 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('books', function (Blueprint $table) {
+        Schema::table(
+            'books', function (Blueprint $table) {
             //
-        });
+        }
+        );
     }
 };
